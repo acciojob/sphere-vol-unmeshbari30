ba75ba60bceb271c5b4f635715286dc8e59bd1f2
Unmesh Bari <unmeshbari5@gmail.com>
2025-12-14 23:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+        event.preventDefault();
+        let inp = document.getElementById('radius')
+        let volumeInp = document.getElementById("volume")
+        let r = Number(inp.value);
+
+        if(isNaN(r) || r<=0){
+            volumeInp.value = "NaN"   
+            return;
+        }
+        let volume = (4/3) * Math.PI * r *r*r;
+        volumeInp.value = volume;
+    
+    }   
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
